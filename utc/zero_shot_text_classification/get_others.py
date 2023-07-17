ee439231d73993a871655d23fed051b49a8d56f7
vic4code <vic4code@gmail.com>
2023-07-17 16:11:03 +0800

    Add anomaly data

--- utc/zero_shot_text_classification/get_others.py
+++ utc/zero_shot_text_classification/get_others.py
@@ -7,16 +7,19 @@ if __name__ == "__main__":
     with open(label_studio_file, "r", encoding="utf-8") as f:
         raw_examples = json.loads(f.read())
 
+    inner_ids = []
     samples = []
     choices = []
     for example in raw_examples:
         if example["annotations"][0]["result"]:
             if "其他" in example["annotations"][0]["result"][0]["value"]["choices"]:
+                inner_ids.append(example["inner_id"])
                 samples.append(example["data"]["jid"])
                 choices.append(example["annotations"][0]["result"][0]["value"]["choices"])
 
 
     df = pd.DataFrame({
+        "inner_id":inner_ids,
         "jid":samples,
         "choices":choices
     }
